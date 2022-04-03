e0ad1d6a4dedd2dce37679657981bfad3c2496fc
diazzj183 <102171309+diazzj183@users.noreply.github.com>
2022-04-03 13:38:35 -0600

    cree una ventana login y le modifique la inertfaz

--- 1-Presentacion/Program.cs
+++ 1-Presentacion/Program.cs
@@ -16,7 +16,7 @@ namespace _1_Presentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
