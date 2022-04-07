1837b30ca10f094e04f760fe45ac3b27c2564b8d
Damian Torres Garcia <damiantg01@outlook.com>
2022-04-07 15:54:50 -0500

    Se agrego mi base de datoas a Configuracion Dmn01

--- 2-Logica/Properties/Settings.Designer.cs
+++ 2-Logica/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace _2_Logica.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -41,6 +41,16 @@ namespace _2_Logica.Properties {
         public string Conexion_Angel {
             get {
                 return ((string)(this["Conexion_Angel"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DAMIANXD;Initial Catalog=DB_DEPORTES;Integrated Security=True")]
+        public string Conexion_Damian {
+            get {
+                return ((string)(this["Conexion_Damian"]));
             }
         }
     }
