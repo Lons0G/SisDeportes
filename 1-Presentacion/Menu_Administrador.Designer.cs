a13c521b42bfde5aeb84fd5dea0143d8e4c34fd9
Lons0G <angel_alonso1@outlook.es>
2022-04-07 15:52:36 -0500

    Se cambio propiedades del menu principal
    Se modifico algunos valores del tamaño de la ventana

--- 1-Presentacion/Menu_Administrador.Designer.cs
+++ 1-Presentacion/Menu_Administrador.Designer.cs
@@ -52,10 +52,10 @@
             this.lbladmi.AutoSize = true;
             this.lbladmi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbladmi.ForeColor = System.Drawing.Color.White;
-            this.lbladmi.Location = new System.Drawing.Point(12, 98);
+            this.lbladmi.Location = new System.Drawing.Point(9, 80);
             this.lbladmi.Margin = new System.Windows.Forms.Padding(0);
             this.lbladmi.Name = "lbladmi";
-            this.lbladmi.Size = new System.Drawing.Size(132, 25);
+            this.lbladmi.Size = new System.Drawing.Size(107, 20);
             this.lbladmi.TabIndex = 3;
             this.lbladmi.Text = "Administrador";
             // 
@@ -72,18 +72,16 @@
             this._p_lateral.Dock = System.Windows.Forms.DockStyle.Left;
             this._p_lateral.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this._p_lateral.Location = new System.Drawing.Point(0, 0);
-            this._p_lateral.Margin = new System.Windows.Forms.Padding(4);
             this._p_lateral.Name = "_p_lateral";
-            this._p_lateral.Size = new System.Drawing.Size(160, 444);
+            this._p_lateral.Size = new System.Drawing.Size(120, 761);
             this._p_lateral.TabIndex = 4;
             // 
             // btnOpcionesUsuario
             // 
             this.btnOpcionesUsuario.BackColor = System.Drawing.Color.Chartreuse;
-            this.btnOpcionesUsuario.Location = new System.Drawing.Point(0, 128);
-            this.btnOpcionesUsuario.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpcionesUsuario.Location = new System.Drawing.Point(0, 104);
             this.btnOpcionesUsuario.Name = "btnOpcionesUsuario";
-            this.btnOpcionesUsuario.Size = new System.Drawing.Size(160, 62);
+            this.btnOpcionesUsuario.Size = new System.Drawing.Size(120, 50);
             this.btnOpcionesUsuario.TabIndex = 8;
             this.btnOpcionesUsuario.Text = "Opciones Usuarios";
             this.btnOpcionesUsuario.UseVisualStyleBackColor = false;
@@ -92,10 +90,9 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.button1.Location = new System.Drawing.Point(0, 198);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(0, 161);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 62);
+            this.button1.Size = new System.Drawing.Size(120, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "Usuarios";
             this.button1.UseVisualStyleBackColor = false;
@@ -104,10 +101,9 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.button3.Location = new System.Drawing.Point(0, 333);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(0, 271);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(160, 62);
+            this.button3.Size = new System.Drawing.Size(120, 50);
             this.button3.TabIndex = 7;
             this.button3.Text = "Reportes";
             this.button3.UseVisualStyleBackColor = false;
@@ -115,10 +111,9 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.button2.Location = new System.Drawing.Point(0, 264);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(0, 214);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(160, 62);
+            this.button2.Size = new System.Drawing.Size(120, 50);
             this.button2.TabIndex = 6;
             this.button2.Text = "Deportes";
             this.button2.UseVisualStyleBackColor = false;
@@ -127,19 +122,17 @@
             // 
             this._p_salir.Controls.Add(this.button4);
             this._p_salir.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this._p_salir.Location = new System.Drawing.Point(0, 401);
-            this._p_salir.Margin = new System.Windows.Forms.Padding(4);
+            this._p_salir.Location = new System.Drawing.Point(0, 726);
             this._p_salir.Name = "_p_salir";
-            this._p_salir.Size = new System.Drawing.Size(160, 43);
+            this._p_salir.Size = new System.Drawing.Size(120, 35);
             this._p_salir.TabIndex = 5;
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.WhiteSmoke;
             this.button4.Location = new System.Drawing.Point(0, 0);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(160, 43);
+            this.button4.Size = new System.Drawing.Size(120, 35);
             this.button4.TabIndex = 8;
             this.button4.Text = "Salir";
             this.button4.UseVisualStyleBackColor = false;
@@ -149,9 +142,9 @@
             // 
             this._p_usuario.Controls.Add(this._pb_imagen_usuario);
             this._p_usuario.Location = new System.Drawing.Point(0, 0);
-            this._p_usuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 12);
+            this._p_usuario.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this._p_usuario.Name = "_p_usuario";
-            this._p_usuario.Size = new System.Drawing.Size(160, 95);
+            this._p_usuario.Size = new System.Drawing.Size(120, 77);
             this._p_usuario.TabIndex = 5;
             // 
             // _pb_imagen_usuario
@@ -159,32 +152,33 @@
             this._pb_imagen_usuario.Dock = System.Windows.Forms.DockStyle.Fill;
             this._pb_imagen_usuario.Image = global::_1_Presentacion.Properties.Resources.usuario_de_perfil;
             this._pb_imagen_usuario.Location = new System.Drawing.Point(0, 0);
-            this._pb_imagen_usuario.Margin = new System.Windows.Forms.Padding(4);
             this._pb_imagen_usuario.Name = "_pb_imagen_usuario";
-            this._pb_imagen_usuario.Size = new System.Drawing.Size(160, 95);
+            this._pb_imagen_usuario.Size = new System.Drawing.Size(120, 77);
             this._pb_imagen_usuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this._pb_imagen_usuario.TabIndex = 5;
             this._pb_imagen_usuario.TabStop = false;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(200, 49);
+            this.pictureBox1.Location = new System.Drawing.Point(120, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(552, 334);
+            this.pictureBox1.Size = new System.Drawing.Size(1104, 761);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // Menu_Administrador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
-            this.ClientSize = new System.Drawing.Size(805, 444);
+            this.ClientSize = new System.Drawing.Size(1224, 761);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this._p_lateral);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Menu_Administrador";
             this.Text = "Menu_Administrador";
             this.Load += new System.EventHandler(this.Menu_Administrador_Load);
