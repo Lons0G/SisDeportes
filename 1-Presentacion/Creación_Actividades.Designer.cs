69eb5e59596ea30ce40c8a6f41b82103608ce4a5
Brandon Reyes <101955462+ReyesKor07@users.noreply.github.com>
2022-04-09 14:12:08 -0500

    Creacion_Actividades Forms
    Se envía el forms y datos de la creación de actividades.

---
+++ 1-Presentacion/Creación_Actividades.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace _1_Presentacion
+{
+    partial class Creación_Actividades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtActividad = new System.Windows.Forms.TextBox();
+            this.cmbDeporte = new System.Windows.Forms.ComboBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.cmbContri1 = new System.Windows.Forms.ComboBox();
+            this.cmbContri2 = new System.Windows.Forms.ComboBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbHora = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(149, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de Actividad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Actividad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Deporte:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Categoria:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fecha:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 246);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 27);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Hora:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 27);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Contricante 1:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 345);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 27);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Contricante 2:";
+            // 
+            // txtActividad
+            // 
+            this.txtActividad.Location = new System.Drawing.Point(168, 33);
+            this.txtActividad.Name = "txtActividad";
+            this.txtActividad.Size = new System.Drawing.Size(407, 35);
+            this.txtActividad.TabIndex = 8;
+            // 
+            // cmbDeporte
+            // 
+            this.cmbDeporte.FormattingEnabled = true;
+            this.cmbDeporte.Location = new System.Drawing.Point(168, 83);
+            this.cmbDeporte.Name = "cmbDeporte";
+            this.cmbDeporte.Size = new System.Drawing.Size(407, 35);
+            this.cmbDeporte.TabIndex = 9;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(168, 137);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(407, 35);
+            this.cmbCategoria.TabIndex = 10;
+            this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.cmbCategoria_SelectedIndexChanged);
+            // 
+            // cmbContri1
+            // 
+            this.cmbContri1.FormattingEnabled = true;
+            this.cmbContri1.Location = new System.Drawing.Point(168, 286);
+            this.cmbContri1.Name = "cmbContri1";
+            this.cmbContri1.Size = new System.Drawing.Size(407, 35);
+            this.cmbContri1.TabIndex = 12;
+            // 
+            // cmbContri2
+            // 
+            this.cmbContri2.FormattingEnabled = true;
+            this.cmbContri2.Location = new System.Drawing.Point(168, 337);
+            this.cmbContri2.Name = "cmbContri2";
+            this.cmbContri2.Size = new System.Drawing.Size(407, 35);
+            this.cmbContri2.TabIndex = 13;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(168, 188);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(407, 35);
+            this.dtpFecha.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.cmbHora);
+            this.groupBox1.Controls.Add(this.btnAceptar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dtpFecha);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbContri2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cmbContri1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.cmbCategoria);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.cmbDeporte);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtActividad);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(48, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(586, 450);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // cmbHora
+            // 
+            this.cmbHora.FormattingEnabled = true;
+            this.cmbHora.Items.AddRange(new object[] {
+            "08.00",
+            "10.00",
+            "12.00",
+            "14.00",
+            "16.00",
+            "18.00",
+            "20.00",
+            "22.00"});
+            this.cmbHora.Location = new System.Drawing.Point(168, 238);
+            this.cmbHora.Name = "cmbHora";
+            this.cmbHora.Size = new System.Drawing.Size(407, 35);
+            this.cmbHora.TabIndex = 17;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.White;
+            this.btnAceptar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnAceptar.Location = new System.Drawing.Point(215, 394);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(107, 50);
+            this.btnAceptar.TabIndex = 16;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // Creación_Actividades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(692, 634);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Creación_Actividades";
+            this.Text = "Actividad";
+            this.Load += new System.EventHandler(this.Creación_Actividades_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtActividad;
+        private System.Windows.Forms.ComboBox cmbDeporte;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.ComboBox cmbContri1;
+        private System.Windows.Forms.ComboBox cmbContri2;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox cmbHora;
+    }
+}
