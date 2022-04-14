4f9bcdcc39307da7410280c65e00e6669124b668
diazzj183 <102171309+diazzj183@users.noreply.github.com>
2022-04-13 20:15:37 -0600

    agrege 2 botones mas (deportes y actividad)

--- 1-Presentacion/Menu_Administrador.cs
+++ 1-Presentacion/Menu_Administrador.cs
@@ -116,7 +116,7 @@ namespace _1_Presentacion
         private void Agregar_BTN_Usuario() {
             //REALIZAR LOS BOTONES FALTANTES PARA EL MENU DEL USUARIO//
 
-            //INSTANCIA UN NUEVO BOTON USUARIO Y LE COLOCA LAS PROPIEDADES//
+            //INSTANCIA UN NUEVO BOTON EQUIPO Y LE COLOCA LAS PROPIEDADES//
             Button btn_Equipos = new Button();
             btn_Equipos.Name = "btn_Equipos";
             btn_Equipos.Text = "Equipos";
@@ -124,21 +124,41 @@ namespace _1_Presentacion
             btn_Equipos.Height = 50;
             btn_Equipos.Top = (50 + 12) * 1;
 
-            //INSTANCIA UN NUEVO BOTON DEPORTES Y LE COLOCA LAS PROPIEDADES
+            //INSTANCIA UN NUEVO BOTON INTEGRANTES Y LE COLOCA LAS PROPIEDADES
             Button btn_integrantes = new Button();
             btn_integrantes.Name = "btn_Integrantes";
             btn_integrantes.Text = "Integrantes";
             btn_integrantes.Width = 120;
             btn_integrantes.Height = 50;
             btn_integrantes.Top = (50 + 12) * 2;
+            
+            //INSTANCIA UN NUEVO BOTON DEPORTES Y LE COLOCA LAS PROPIEDADES
+            Button btn_deportes = new Button();
+            btn_deportes.Name = "btn_deportes";
+            btn_deportes.Text = "Deportes";
+            btn_deportes.Width = 120;
+            btn_deportes.Height = 50;
+            btn_deportes.Top = (50 + 12) * 3;
+
+            //INSTANCIA UN NUEVO BOTON ACTIVIDAD Y LE COLOCA LAS PROPIEDADES
+            Button btn_actividad = new Button();
+            btn_actividad.Name = "btn_deportes";
+            btn_actividad.Text = "Deportes";
+            btn_actividad.Width = 120;
+            btn_actividad.Height = 50;
+            btn_actividad.Top = (50 + 12) * 4;
 
             //GENERA UN NUEVO METEDO CUANDO SE DE CLICK AL BOTON USUARIO//
             btn_Equipos.Click += Btn_Equipos_Click;
             btn_integrantes.Click += Btn_Integrantes_Click;
+            btn_deportes.Click += Btn_Deportes_Click;
+            btn_actividad.Click += Btn_Deportes_Click;
 
             //SE AGREGA EL BOTON USUARIO AL PANEL DE CONTROLES//
             _p_controles.Controls.Add(btn_Equipos);
             _p_controles.Controls.Add(btn_integrantes);
+            _p_controles.Controls.Add(btn_deportes);
+            _p_controles.Controls.Add(btn_actividad);
         }
     }
 }
