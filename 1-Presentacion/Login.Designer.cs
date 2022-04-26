1db4c0bd37d853215c873a4be6330893b85d78a4
Tomate26 <a2193720104@alumnos.uat.edu.mx>
2022-04-26 02:43:34 -0500

    Actualización de diseño login

--- 1-Presentacion/Login.Designer.cs
+++ 1-Presentacion/Login.Designer.cs
@@ -29,46 +29,33 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.panel1 = new System.Windows.Forms.Panel();
             this.pbmostrar = new System.Windows.Forms.PictureBox();
             this.pbocultar = new System.Windows.Forms.PictureBox();
             this.btnIniciar = new System.Windows.Forms.Button();
             this.txtContraLog = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtUsuLog = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pbmostrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbocultar)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.pbmostrar);
-            this.panel1.Controls.Add(this.pbocultar);
-            this.panel1.Controls.Add(this.btnIniciar);
-            this.panel1.Controls.Add(this.txtContraLog);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 180);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(417, 136);
-            this.panel1.TabIndex = 24;
             // 
             // pbmostrar
             // 
             this.pbmostrar.Image = ((System.Drawing.Image)(resources.GetObject("pbmostrar.Image")));
-            this.pbmostrar.Location = new System.Drawing.Point(155, 15);
-            this.pbmostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbmostrar.Location = new System.Drawing.Point(100, 378);
+            this.pbmostrar.Margin = new System.Windows.Forms.Padding(2);
             this.pbmostrar.Name = "pbmostrar";
-            this.pbmostrar.Size = new System.Drawing.Size(40, 25);
+            this.pbmostrar.Size = new System.Drawing.Size(26, 27);
             this.pbmostrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbmostrar.TabIndex = 34;
             this.pbmostrar.TabStop = false;
@@ -77,10 +64,10 @@
             // pbocultar
             // 
             this.pbocultar.Image = ((System.Drawing.Image)(resources.GetObject("pbocultar.Image")));
-            this.pbocultar.Location = new System.Drawing.Point(155, 15);
-            this.pbocultar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbocultar.Location = new System.Drawing.Point(100, 378);
+            this.pbocultar.Margin = new System.Windows.Forms.Padding(2);
             this.pbocultar.Name = "pbocultar";
-            this.pbocultar.Size = new System.Drawing.Size(40, 24);
+            this.pbocultar.Size = new System.Drawing.Size(26, 26);
             this.pbocultar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbocultar.TabIndex = 33;
             this.pbocultar.TabStop = false;
@@ -88,25 +75,25 @@
             // 
             // btnIniciar
             // 
-            this.btnIniciar.BackColor = System.Drawing.Color.RosyBrown;
-            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnIniciar.Location = new System.Drawing.Point(244, 61);
-            this.btnIniciar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnIniciar.BackColor = System.Drawing.Color.Firebrick;
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnIniciar.Location = new System.Drawing.Point(94, 480);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(2);
             this.btnIniciar.Name = "btnIniciar";
-            this.btnIniciar.Size = new System.Drawing.Size(84, 37);
+            this.btnIniciar.Size = new System.Drawing.Size(155, 76);
             this.btnIniciar.TabIndex = 14;
-            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.Text = "INICIAR";
             this.btnIniciar.UseVisualStyleBackColor = false;
             this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
             // txtContraLog
             // 
-            this.txtContraLog.BackColor = System.Drawing.SystemColors.Info;
+            this.txtContraLog.BackColor = System.Drawing.SystemColors.HighlightText;
             this.txtContraLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtContraLog.ForeColor = System.Drawing.Color.Black;
-            this.txtContraLog.Location = new System.Drawing.Point(207, 15);
-            this.txtContraLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtContraLog.Location = new System.Drawing.Point(144, 378);
+            this.txtContraLog.Margin = new System.Windows.Forms.Padding(2);
             this.txtContraLog.Name = "txtContraLog";
             this.txtContraLog.PasswordChar = '*';
             this.txtContraLog.Size = new System.Drawing.Size(122, 26);
@@ -115,45 +102,34 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(25, 20);
+            this.label2.Location = new System.Drawing.Point(96, 350);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.Size = new System.Drawing.Size(95, 20);
             this.label2.TabIndex = 12;
             this.label2.Text = "Contraseña:";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(118, 55);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(110, 82);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 23;
-            this.pictureBox2.TabStop = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.Info;
-            this.label3.Location = new System.Drawing.Point(98, 21);
+            this.label3.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(85, 47);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(148, 31);
+            this.label3.Size = new System.Drawing.Size(189, 32);
             this.label3.TabIndex = 22;
-            this.label3.Text = "Bienvenido";
+            this.label3.Text = "BIENVENIDO";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(254, 7);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(102, 106);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(130, 53);
+            this.pictureBox1.Size = new System.Drawing.Size(147, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 21;
             this.pictureBox1.TabStop = false;
@@ -161,64 +137,104 @@
             // txtUsuLog
             // 
             this.txtUsuLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuLog.Location = new System.Drawing.Point(207, 146);
-            this.txtUsuLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUsuLog.Location = new System.Drawing.Point(100, 285);
+            this.txtUsuLog.Margin = new System.Windows.Forms.Padding(2);
             this.txtUsuLog.Name = "txtUsuLog";
-            this.txtUsuLog.Size = new System.Drawing.Size(122, 26);
+            this.txtUsuLog.Size = new System.Drawing.Size(166, 26);
             this.txtUsuLog.TabIndex = 20;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(35, 146);
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(97, 259);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.Size = new System.Drawing.Size(68, 20);
             this.label1.TabIndex = 19;
             this.label1.Text = "Usuario:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(62, 622);
+            this.panel1.TabIndex = 35;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(300, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(288, 622);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 36;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(64, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(238, 256);
+            this.panel2.TabIndex = 37;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MistyRose;
+            this.panel3.Location = new System.Drawing.Point(63, 247);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(236, 195);
+            this.panel3.TabIndex = 37;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Maroon;
-            this.ClientSize = new System.Drawing.Size(417, 316);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(587, 620);
             this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pbocultar);
+            this.Controls.Add(this.pbmostrar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtContraLog);
             this.Controls.Add(this.txtUsuLog);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbmostrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbocultar)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnIniciar;
         private System.Windows.Forms.TextBox txtContraLog;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtUsuLog;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pbmostrar;
         private System.Windows.Forms.PictureBox pbocultar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
