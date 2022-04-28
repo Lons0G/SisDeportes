e19a9f95b35bfa42954069cdd57c54ebb8a34901
Brandon Reyes <101955462+ReyesKor07@users.noreply.github.com>
2022-04-28 12:49:13 -0500

    -----
    -----

--- 2-Logica/CLS_L_Categoria.cs
+++ 2-Logica/CLS_L_Categoria.cs
@@ -8,8 +8,7 @@ namespace _2_Logica
     public class CLS_L_Categoria
     {
         SqlConnection _conexion;
-        //private void Conexion() { _conexion = new SqlConnection(Properties.Settings.Default.Conexion_Damian); }
-        private void Conexion() { _conexion = new SqlConnection(Properties.Settings.Default.Conexion_Brandon); }
+        private void Conexion() { _conexion = new SqlConnection(Properties.Settings.Default.Conexion_Damian); }
 
         public bool Insertar_Categoria(ref CLS_Categoria obj_categoria) {
             try {
