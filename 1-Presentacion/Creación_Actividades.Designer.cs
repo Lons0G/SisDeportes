643cf900f9be7f512375cf7f41e5a46801905390
Lons0G <angel_alonso1@outlook.es>
2022-04-28 16:38:20 -0500

    Se modifico la ventana de creacion actividad

--- 1-Presentacion/Creación_Actividades.Designer.cs
+++ 1-Presentacion/Creación_Actividades.Designer.cs
@@ -50,108 +50,109 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(154, 31);
+            this.label1.Location = new System.Drawing.Point(121, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(387, 45);
+            this.label1.Size = new System.Drawing.Size(267, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Registro de Actividad";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Location = new System.Drawing.Point(29, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 27);
+            this.label2.Size = new System.Drawing.Size(70, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Actividad:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 89);
+            this.label4.Location = new System.Drawing.Point(29, 122);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(111, 27);
+            this.label4.Size = new System.Drawing.Size(71, 19);
             this.label4.TabIndex = 3;
             this.label4.Text = "Categoria:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 145);
+            this.label5.Location = new System.Drawing.Point(29, 202);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 27);
+            this.label5.Size = new System.Drawing.Size(49, 19);
             this.label5.TabIndex = 4;
             this.label5.Text = "Fecha:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 195);
+            this.label6.Location = new System.Drawing.Point(29, 279);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(66, 27);
+            this.label6.Size = new System.Drawing.Size(43, 19);
             this.label6.TabIndex = 5;
             this.label6.Text = "Hora:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 243);
+            this.label7.Location = new System.Drawing.Point(29, 353);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(149, 27);
+            this.label7.Size = new System.Drawing.Size(94, 19);
             this.label7.TabIndex = 6;
             this.label7.Text = "Contricante 1:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 294);
+            this.label8.Location = new System.Drawing.Point(29, 429);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(149, 27);
+            this.label8.Size = new System.Drawing.Size(94, 19);
             this.label8.TabIndex = 7;
             this.label8.Text = "Contricante 2:";
             // 
             // txtActividad
             // 
-            this.txtActividad.Location = new System.Drawing.Point(168, 33);
+            this.txtActividad.Location = new System.Drawing.Point(105, 54);
             this.txtActividad.Name = "txtActividad";
-            this.txtActividad.Size = new System.Drawing.Size(407, 35);
+            this.txtActividad.Size = new System.Drawing.Size(370, 26);
             this.txtActividad.TabIndex = 8;
             // 
             // cmbCategoria
             // 
             this.cmbCategoria.FormattingEnabled = true;
-            this.cmbCategoria.Location = new System.Drawing.Point(168, 86);
+            this.cmbCategoria.Location = new System.Drawing.Point(106, 119);
             this.cmbCategoria.Name = "cmbCategoria";
-            this.cmbCategoria.Size = new System.Drawing.Size(407, 35);
+            this.cmbCategoria.Size = new System.Drawing.Size(369, 27);
             this.cmbCategoria.TabIndex = 10;
             // 
             // cmbContri1
             // 
             this.cmbContri1.FormattingEnabled = true;
-            this.cmbContri1.Location = new System.Drawing.Point(168, 235);
+            this.cmbContri1.Location = new System.Drawing.Point(129, 350);
             this.cmbContri1.Name = "cmbContri1";
-            this.cmbContri1.Size = new System.Drawing.Size(407, 35);
+            this.cmbContri1.Size = new System.Drawing.Size(346, 27);
             this.cmbContri1.TabIndex = 12;
             // 
             // cmbContri2
             // 
             this.cmbContri2.FormattingEnabled = true;
-            this.cmbContri2.Location = new System.Drawing.Point(168, 286);
+            this.cmbContri2.Location = new System.Drawing.Point(129, 426);
             this.cmbContri2.Name = "cmbContri2";
-            this.cmbContri2.Size = new System.Drawing.Size(407, 35);
+            this.cmbContri2.Size = new System.Drawing.Size(346, 27);
             this.cmbContri2.TabIndex = 13;
             // 
             // dtpFecha
             // 
-            this.dtpFecha.Location = new System.Drawing.Point(168, 137);
+            this.dtpFecha.Location = new System.Drawing.Point(84, 196);
             this.dtpFecha.Name = "dtpFecha";
-            this.dtpFecha.Size = new System.Drawing.Size(407, 35);
+            this.dtpFecha.Size = new System.Drawing.Size(391, 26);
             this.dtpFecha.TabIndex = 14;
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.BackColor = System.Drawing.Color.LightCoral;
             this.groupBox1.Controls.Add(this.dtpHora);
+            this.groupBox1.Controls.Add(this.btnAceptar);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.dtpFecha);
             this.groupBox1.Controls.Add(this.label4);
@@ -163,10 +164,10 @@
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.txtActividad);
-            this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(48, 98);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(-2, 98);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(586, 341);
+            this.groupBox1.Size = new System.Drawing.Size(518, 585);
             this.groupBox1.TabIndex = 16;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos";
@@ -174,16 +175,16 @@
             // dtpHora
             // 
             this.dtpHora.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpHora.Location = new System.Drawing.Point(168, 189);
+            this.dtpHora.Location = new System.Drawing.Point(78, 273);
             this.dtpHora.Name = "dtpHora";
-            this.dtpHora.Size = new System.Drawing.Size(407, 35);
+            this.dtpHora.Size = new System.Drawing.Size(397, 26);
             this.dtpHora.TabIndex = 20;
             // 
             // btnAceptar
             // 
             this.btnAceptar.BackColor = System.Drawing.Color.White;
             this.btnAceptar.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnAceptar.Location = new System.Drawing.Point(287, 493);
+            this.btnAceptar.Location = new System.Drawing.Point(206, 495);
             this.btnAceptar.Name = "btnAceptar";
             this.btnAceptar.Size = new System.Drawing.Size(107, 50);
             this.btnAceptar.TabIndex = 16;
@@ -193,12 +194,11 @@
             // 
             // Creación_Actividades
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkRed;
-            this.ClientSize = new System.Drawing.Size(692, 613);
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(517, 681);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
