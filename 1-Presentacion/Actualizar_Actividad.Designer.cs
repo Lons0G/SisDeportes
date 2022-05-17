4cf8a3d25f60c9a7c48bde88b7e54d804dac48bc
Tomate26 <a2193720104@alumnos.uat.edu.mx>
2022-05-17 15:03:58 -0500

    Cambio diseño del form ActualizarActividad

--- 1-Presentacion/Actualizar_Actividad.Designer.cs
+++ 1-Presentacion/Actualizar_Actividad.Designer.cs
@@ -48,33 +48,37 @@
             this.txtActividad = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnCargar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.LightCoral;
+            this.groupBox1.BackColor = System.Drawing.Color.MistyRose;
             this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Controls.Add(this.cboTorneo);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.cboAct);
-            this.groupBox1.Controls.Add(this.dtpHora);
-            this.groupBox1.Controls.Add(this.btnAceptar);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.dtpFecha);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.cmbContri2);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.cmbContri1);
             this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.cmbCategoria);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.cboTorneo);
+            this.groupBox1.Controls.Add(this.cboAct);
+            this.groupBox1.Controls.Add(this.dtpHora);
+            this.groupBox1.Controls.Add(this.dtpFecha);
+            this.groupBox1.Controls.Add(this.cmbContri2);
+            this.groupBox1.Controls.Add(this.cmbContri1);
+            this.groupBox1.Controls.Add(this.cmbCategoria);
             this.groupBox1.Controls.Add(this.txtActividad);
             this.groupBox1.ForeColor = System.Drawing.Color.Black;
-            this.groupBox1.Location = new System.Drawing.Point(2, 110);
+            this.groupBox1.Location = new System.Drawing.Point(1, 89);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(518, 555);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(389, 488);
             this.groupBox1.TabIndex = 17;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos";
@@ -82,185 +86,239 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(17, 436);
+            this.label9.BackColor = System.Drawing.Color.LightCoral;
+            this.label9.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label9.Location = new System.Drawing.Point(2, 370);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(104, 16);
+            this.label9.Size = new System.Drawing.Size(112, 20);
             this.label9.TabIndex = 24;
             this.label9.Text = "Tipo de Torneo:";
             // 
             // cboTorneo
             // 
             this.cboTorneo.FormattingEnabled = true;
-            this.cboTorneo.Location = new System.Drawing.Point(129, 433);
+            this.cboTorneo.Location = new System.Drawing.Point(158, 373);
+            this.cboTorneo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboTorneo.Name = "cboTorneo";
-            this.cboTorneo.Size = new System.Drawing.Size(346, 24);
+            this.cboTorneo.Size = new System.Drawing.Size(201, 21);
             this.cboTorneo.TabIndex = 23;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 105);
+            this.label3.BackColor = System.Drawing.Color.LightCoral;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label3.Location = new System.Drawing.Point(6, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 16);
+            this.label3.Size = new System.Drawing.Size(109, 20);
             this.label3.TabIndex = 22;
             this.label3.Text = "Tipo Actividad:";
             // 
             // cboAct
             // 
             this.cboAct.FormattingEnabled = true;
-            this.cboAct.Location = new System.Drawing.Point(130, 102);
+            this.cboAct.Location = new System.Drawing.Point(157, 104);
+            this.cboAct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboAct.Name = "cboAct";
-            this.cboAct.Size = new System.Drawing.Size(346, 24);
+            this.cboAct.Size = new System.Drawing.Size(203, 21);
             this.cboAct.TabIndex = 21;
             // 
             // dtpHora
             // 
             this.dtpHora.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpHora.Location = new System.Drawing.Point(128, 251);
+            this.dtpHora.Location = new System.Drawing.Point(157, 225);
+            this.dtpHora.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpHora.Name = "dtpHora";
-            this.dtpHora.Size = new System.Drawing.Size(347, 22);
+            this.dtpHora.Size = new System.Drawing.Size(202, 20);
             this.dtpHora.TabIndex = 20;
             // 
             // btnAceptar
             // 
-            this.btnAceptar.BackColor = System.Drawing.Color.White;
-            this.btnAceptar.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnAceptar.Location = new System.Drawing.Point(369, 478);
+            this.btnAceptar.BackColor = System.Drawing.Color.Brown;
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCoral;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Location = new System.Drawing.Point(-1, 437);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(107, 50);
+            this.btnAceptar.Size = new System.Drawing.Size(127, 53);
             this.btnAceptar.TabIndex = 16;
-            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.Text = "ACEPTAR";
             this.btnAceptar.UseVisualStyleBackColor = false;
             this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 61);
+            this.label2.BackColor = System.Drawing.Color.LightCoral;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label2.Location = new System.Drawing.Point(6, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.Size = new System.Drawing.Size(75, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Actividad:";
             // 
             // dtpFecha
             // 
-            this.dtpFecha.Location = new System.Drawing.Point(128, 198);
+            this.dtpFecha.Location = new System.Drawing.Point(157, 182);
+            this.dtpFecha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpFecha.Name = "dtpFecha";
-            this.dtpFecha.Size = new System.Drawing.Size(348, 22);
+            this.dtpFecha.Size = new System.Drawing.Size(203, 20);
             this.dtpFecha.TabIndex = 14;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(29, 154);
+            this.label4.BackColor = System.Drawing.Color.LightCoral;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label4.Location = new System.Drawing.Point(11, 141);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.Size = new System.Drawing.Size(77, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Categoria:";
             // 
             // cmbContri2
             // 
             this.cmbContri2.FormattingEnabled = true;
-            this.cmbContri2.Location = new System.Drawing.Point(129, 379);
+            this.cmbContri2.Location = new System.Drawing.Point(158, 329);
+            this.cmbContri2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbContri2.Name = "cmbContri2";
-            this.cmbContri2.Size = new System.Drawing.Size(346, 24);
+            this.cmbContri2.Size = new System.Drawing.Size(201, 21);
             this.cmbContri2.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(30, 204);
+            this.label5.BackColor = System.Drawing.Color.LightCoral;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label5.Location = new System.Drawing.Point(11, 182);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.Size = new System.Drawing.Size(49, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Fecha:";
             // 
             // cmbContri1
             // 
             this.cmbContri1.FormattingEnabled = true;
-            this.cmbContri1.Location = new System.Drawing.Point(128, 311);
+            this.cmbContri1.Location = new System.Drawing.Point(157, 274);
+            this.cmbContri1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbContri1.Name = "cmbContri1";
-            this.cmbContri1.Size = new System.Drawing.Size(346, 24);
+            this.cmbContri1.Size = new System.Drawing.Size(201, 21);
             this.cmbContri1.TabIndex = 12;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(30, 258);
+            this.label6.BackColor = System.Drawing.Color.LightCoral;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label6.Location = new System.Drawing.Point(11, 226);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(40, 16);
+            this.label6.Size = new System.Drawing.Size(45, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "Hora:";
             // 
             // cmbCategoria
             // 
             this.cmbCategoria.FormattingEnabled = true;
-            this.cmbCategoria.Location = new System.Drawing.Point(130, 151);
+            this.cmbCategoria.Location = new System.Drawing.Point(159, 144);
+            this.cmbCategoria.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbCategoria.Name = "cmbCategoria";
-            this.cmbCategoria.Size = new System.Drawing.Size(347, 24);
+            this.cmbCategoria.Size = new System.Drawing.Size(202, 21);
             this.cmbCategoria.TabIndex = 10;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(28, 314);
+            this.label7.BackColor = System.Drawing.Color.LightCoral;
+            this.label7.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label7.Location = new System.Drawing.Point(10, 271);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(87, 16);
+            this.label7.Size = new System.Drawing.Size(100, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Contricante 1:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(29, 382);
+            this.label8.BackColor = System.Drawing.Color.LightCoral;
+            this.label8.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F);
+            this.label8.Location = new System.Drawing.Point(11, 326);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(87, 16);
+            this.label8.Size = new System.Drawing.Size(100, 20);
             this.label8.TabIndex = 7;
             this.label8.Text = "Contricante 2:";
             // 
             // txtActividad
             // 
-            this.txtActividad.Location = new System.Drawing.Point(128, 58);
+            this.txtActividad.Location = new System.Drawing.Point(157, 68);
+            this.txtActividad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtActividad.Name = "txtActividad";
-            this.txtActividad.Size = new System.Drawing.Size(348, 22);
+            this.txtActividad.Size = new System.Drawing.Size(203, 20);
             this.txtActividad.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(76, 26);
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(313, 38);
+            this.label1.Size = new System.Drawing.Size(261, 37);
             this.label1.TabIndex = 18;
             this.label1.Text = "Actualizar Actividad";
             // 
             // btnCargar
             // 
-            this.btnCargar.BackColor = System.Drawing.Color.White;
-            this.btnCargar.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnCargar.Location = new System.Drawing.Point(394, 69);
+            this.btnCargar.BackColor = System.Drawing.Color.Brown;
+            this.btnCargar.FlatAppearance.BorderSize = 0;
+            this.btnCargar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCoral;
+            this.btnCargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargar.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCargar.ForeColor = System.Drawing.Color.White;
+            this.btnCargar.Location = new System.Drawing.Point(-1, 1);
+            this.btnCargar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCargar.Name = "btnCargar";
-            this.btnCargar.Size = new System.Drawing.Size(85, 35);
+            this.btnCargar.Size = new System.Drawing.Size(126, 47);
             this.btnCargar.TabIndex = 25;
-            this.btnCargar.Text = "Cargar";
+            this.btnCargar.Text = "CARGAR";
             this.btnCargar.UseVisualStyleBackColor = false;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.btnCargar);
+            this.panel1.Controls.Add(this.btnAceptar);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(126, 488);
+            this.panel1.TabIndex = 25;
+            // 
             // Actualizar_Actividad
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Salmon;
-            this.ClientSize = new System.Drawing.Size(520, 709);
-            this.Controls.Add(this.btnCargar);
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(390, 576);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Actualizar_Actividad";
             this.Text = "Actualizar_Actividad";
             this.Load += new System.EventHandler(this.Actualizar_Actividad_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -288,5 +346,6 @@
         private System.Windows.Forms.TextBox txtActividad;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Panel panel1;
     }
 }
