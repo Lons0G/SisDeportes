2a281587cd9c900fb49aca01126c9d998fa6d69d
Tomate26 <a2193720104@alumnos.uat.edu.mx>
2022-05-17 15:21:00 -0500

    Cambio diseño form TipoAct

--- 1-Presentacion/Actualizar_Tipo_Act.Designer.cs
+++ 1-Presentacion/Actualizar_Tipo_Act.Designer.cs
@@ -37,85 +37,103 @@
             this.btnCargar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.lbl_id = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnActualizar
             // 
-            this.btnActualizar.Location = new System.Drawing.Point(345, 225);
-            this.btnActualizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnActualizar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnActualizar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnActualizar.FlatAppearance.BorderSize = 2;
+            this.btnActualizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActualizar.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnActualizar.Location = new System.Drawing.Point(102, 234);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(80, 26);
+            this.btnActualizar.Size = new System.Drawing.Size(99, 43);
             this.btnActualizar.TabIndex = 58;
-            this.btnActualizar.Text = "Actualizar";
-            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Text = "ACTUALIZAR";
+            this.btnActualizar.UseVisualStyleBackColor = false;
             this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(70, 29);
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(9, 10);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(394, 32);
+            this.label6.Size = new System.Drawing.Size(282, 30);
             this.label6.TabIndex = 57;
             this.label6.Text = "Actualizar Tipo de Actividad";
             // 
             // txtDescrip
             // 
             this.txtDescrip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDescrip.Location = new System.Drawing.Point(264, 164);
-            this.txtDescrip.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDescrip.Location = new System.Drawing.Point(176, 174);
+            this.txtDescrip.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDescrip.Name = "txtDescrip";
-            this.txtDescrip.Size = new System.Drawing.Size(161, 30);
+            this.txtDescrip.Size = new System.Drawing.Size(122, 26);
             this.txtDescrip.TabIndex = 56;
             // 
             // fdd
             // 
             this.fdd.AutoSize = true;
-            this.fdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fdd.Location = new System.Drawing.Point(42, 164);
+            this.fdd.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.fdd.Location = new System.Drawing.Point(11, 176);
+            this.fdd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.fdd.Name = "fdd";
-            this.fdd.Size = new System.Drawing.Size(132, 25);
+            this.fdd.Size = new System.Drawing.Size(100, 21);
             this.fdd.TabIndex = 55;
             this.fdd.Text = "Descripcion:";
             // 
             // txtnombreAct
             // 
             this.txtnombreAct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtnombreAct.Location = new System.Drawing.Point(264, 127);
-            this.txtnombreAct.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtnombreAct.Location = new System.Drawing.Point(176, 133);
+            this.txtnombreAct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnombreAct.Name = "txtnombreAct";
-            this.txtnombreAct.Size = new System.Drawing.Size(161, 30);
+            this.txtnombreAct.Size = new System.Drawing.Size(122, 26);
             this.txtnombreAct.TabIndex = 54;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(42, 127);
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(11, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 25);
+            this.label4.Size = new System.Drawing.Size(75, 21);
             this.label4.TabIndex = 53;
             this.label4.Text = "Nombre:";
             // 
             // btnCargar
             // 
-            this.btnCargar.Location = new System.Drawing.Point(345, 76);
-            this.btnCargar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCargar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCargar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCargar.FlatAppearance.BorderSize = 2;
+            this.btnCargar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargar.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnCargar.Location = new System.Drawing.Point(224, 76);
+            this.btnCargar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCargar.Name = "btnCargar";
-            this.btnCargar.Size = new System.Drawing.Size(80, 26);
+            this.btnCargar.Size = new System.Drawing.Size(74, 33);
             this.btnCargar.TabIndex = 59;
-            this.btnCargar.Text = "Cargar";
-            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Text = "CARGAR";
+            this.btnCargar.UseVisualStyleBackColor = false;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(42, 77);
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(11, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 25);
+            this.label1.Size = new System.Drawing.Size(28, 21);
             this.label1.TabIndex = 60;
             this.label1.Text = "id:";
             // 
@@ -123,28 +141,42 @@
             // 
             this.lbl_id.AutoSize = true;
             this.lbl_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_id.Location = new System.Drawing.Point(100, 77);
+            this.lbl_id.Location = new System.Drawing.Point(43, 82);
+            this.lbl_id.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_id.Name = "lbl_id";
-            this.lbl_id.Size = new System.Drawing.Size(0, 25);
+            this.lbl_id.Size = new System.Drawing.Size(0, 20);
             this.lbl_id.TabIndex = 61;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 54);
+            this.panel1.TabIndex = 62;
             // 
             // Actualizar_Tipo_Act
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 355);
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(305, 288);
             this.Controls.Add(this.lbl_id);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.btnActualizar);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtDescrip);
             this.Controls.Add(this.fdd);
             this.Controls.Add(this.txtnombreAct);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Actualizar_Tipo_Act";
             this.Text = "Actualizar_Tipo_Act";
             this.Load += new System.EventHandler(this.Actualizar_Tipo_Act_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,5 +193,6 @@
         private System.Windows.Forms.Button btnCargar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.Panel panel1;
     }
 }
