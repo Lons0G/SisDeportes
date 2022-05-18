bcab497056aa61af85156609c9b176b51727d2ea
Tomate26 <a2193720104@alumnos.uat.edu.mx>
2022-05-17 22:43:16 -0500

    Todos los forms de Torneos actualizados

--- 1-Presentacion/Opciones_Torneo.Designer.cs
+++ 1-Presentacion/Opciones_Torneo.Designer.cs
@@ -41,15 +41,15 @@
             this._p_torneos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this._p_torneos.BackColor = System.Drawing.Color.RosyBrown;
-            this._p_torneos.Location = new System.Drawing.Point(14, 108);
+            this._p_torneos.BackColor = System.Drawing.Color.LightCoral;
+            this._p_torneos.Location = new System.Drawing.Point(11, 110);
             this._p_torneos.Name = "_p_torneos";
             this._p_torneos.Size = new System.Drawing.Size(330, 280);
             this._p_torneos.TabIndex = 13;
             // 
             // _p_forms2
             // 
-            this._p_forms2.BackColor = System.Drawing.SystemColors.Control;
+            this._p_forms2.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this._p_forms2.Dock = System.Windows.Forms.DockStyle.Right;
             this._p_forms2.Location = new System.Drawing.Point(350, 0);
             this._p_forms2.Name = "_p_forms2";
@@ -60,24 +60,29 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(29, 5);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(30, 5);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(75, 41);
+            this.pictureBox2.Size = new System.Drawing.Size(92, 64);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 11;
             this.pictureBox2.TabStop = false;
             // 
             // btnInsertarTorneo
             // 
-            this.btnInsertarTorneo.BackColor = System.Drawing.Color.Maroon;
+            this.btnInsertarTorneo.BackColor = System.Drawing.Color.IndianRed;
+            this.btnInsertarTorneo.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnInsertarTorneo.FlatAppearance.BorderSize = 2;
+            this.btnInsertarTorneo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.btnInsertarTorneo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInsertarTorneo.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInsertarTorneo.ForeColor = System.Drawing.Color.White;
-            this.btnInsertarTorneo.Location = new System.Drawing.Point(20, 51);
-            this.btnInsertarTorneo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnInsertarTorneo.Location = new System.Drawing.Point(12, 72);
+            this.btnInsertarTorneo.Margin = new System.Windows.Forms.Padding(2);
             this.btnInsertarTorneo.Name = "btnInsertarTorneo";
-            this.btnInsertarTorneo.Size = new System.Drawing.Size(92, 32);
+            this.btnInsertarTorneo.Size = new System.Drawing.Size(124, 36);
             this.btnInsertarTorneo.TabIndex = 10;
-            this.btnInsertarTorneo.Text = "Insertar Torneo";
+            this.btnInsertarTorneo.Text = "INSERTAR TORNEO";
             this.btnInsertarTorneo.UseVisualStyleBackColor = false;
             this.btnInsertarTorneo.Click += new System.EventHandler(this.btnInsertarTorneo_Click);
             // 
@@ -85,14 +90,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackColor = System.Drawing.Color.MistyRose;
             this.ClientSize = new System.Drawing.Size(800, 400);
             this.Controls.Add(this._p_torneos);
             this.Controls.Add(this._p_forms2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.btnInsertarTorneo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Opciones_Torneo";
             this.Text = "Opciones_Torneo";
             this.Load += new System.EventHandler(this.Opciones_Torneo_Load);
--- 1-Presentacion/Torneo.Designer.cs
+++ 1-Presentacion/Torneo.Designer.cs
@@ -38,44 +38,49 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this._cb_tipo = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(92, 9);
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(85, 27);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(226, 26);
+            this.label6.Size = new System.Drawing.Size(222, 31);
             this.label6.TabIndex = 55;
             this.label6.Text = "Registro de Torneos";
             // 
             // fdd
             // 
             this.fdd.AutoSize = true;
-            this.fdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fdd.Location = new System.Drawing.Point(37, 114);
+            this.fdd.BackColor = System.Drawing.Color.Firebrick;
+            this.fdd.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fdd.ForeColor = System.Drawing.Color.White;
+            this.fdd.Location = new System.Drawing.Point(31, 144);
             this.fdd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.fdd.Name = "fdd";
-            this.fdd.Size = new System.Drawing.Size(76, 20);
+            this.fdd.Size = new System.Drawing.Size(75, 21);
             this.fdd.TabIndex = 53;
             this.fdd.Text = "Nombre:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(37, 83);
+            this.label4.BackColor = System.Drawing.Color.Firebrick;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(31, 113);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.Size = new System.Drawing.Size(55, 21);
             this.label4.TabIndex = 51;
             this.label4.Text = "Fecha:";
             // 
             // dtfechatorneo
             // 
-            this.dtfechatorneo.Location = new System.Drawing.Point(153, 83);
+            this.dtfechatorneo.Location = new System.Drawing.Point(147, 113);
             this.dtfechatorneo.Margin = new System.Windows.Forms.Padding(2);
             this.dtfechatorneo.Name = "dtfechatorneo";
             this.dtfechatorneo.Size = new System.Drawing.Size(202, 20);
@@ -83,18 +88,25 @@
             // 
             // btntorneocrear
             // 
-            this.btntorneocrear.Location = new System.Drawing.Point(304, 206);
+            this.btntorneocrear.BackColor = System.Drawing.Color.LightCoral;
+            this.btntorneocrear.FlatAppearance.BorderColor = System.Drawing.Color.Firebrick;
+            this.btntorneocrear.FlatAppearance.BorderSize = 2;
+            this.btntorneocrear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Firebrick;
+            this.btntorneocrear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btntorneocrear.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntorneocrear.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btntorneocrear.Location = new System.Drawing.Point(136, 291);
             this.btntorneocrear.Margin = new System.Windows.Forms.Padding(2);
             this.btntorneocrear.Name = "btntorneocrear";
-            this.btntorneocrear.Size = new System.Drawing.Size(51, 20);
+            this.btntorneocrear.Size = new System.Drawing.Size(104, 48);
             this.btntorneocrear.TabIndex = 57;
-            this.btntorneocrear.Text = "Crear";
-            this.btntorneocrear.UseVisualStyleBackColor = true;
+            this.btntorneocrear.Text = "CREAR";
+            this.btntorneocrear.UseVisualStyleBackColor = false;
             this.btntorneocrear.Click += new System.EventHandler(this.btntorneocrear_Click);
             // 
             // txtnom
             // 
-            this.txtnom.Location = new System.Drawing.Point(153, 114);
+            this.txtnom.Location = new System.Drawing.Point(147, 144);
             this.txtnom.Margin = new System.Windows.Forms.Padding(2);
             this.txtnom.Name = "txtnom";
             this.txtnom.Size = new System.Drawing.Size(202, 20);
@@ -103,7 +115,7 @@
             // _cb_categoria
             // 
             this._cb_categoria.FormattingEnabled = true;
-            this._cb_categoria.Location = new System.Drawing.Point(153, 148);
+            this._cb_categoria.Location = new System.Drawing.Point(147, 178);
             this._cb_categoria.Name = "_cb_categoria";
             this._cb_categoria.Size = new System.Drawing.Size(202, 21);
             this._cb_categoria.TabIndex = 59;
@@ -111,39 +123,51 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(37, 149);
+            this.label1.BackColor = System.Drawing.Color.Firebrick;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 179);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.Size = new System.Drawing.Size(85, 21);
             this.label1.TabIndex = 60;
             this.label1.Text = "Categoria:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(37, 181);
+            this.label2.BackColor = System.Drawing.Color.Firebrick;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(31, 211);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.Size = new System.Drawing.Size(47, 21);
             this.label2.TabIndex = 61;
             this.label2.Text = "Tipo:";
             // 
             // _cb_tipo
             // 
             this._cb_tipo.FormattingEnabled = true;
-            this._cb_tipo.Location = new System.Drawing.Point(153, 180);
+            this._cb_tipo.Location = new System.Drawing.Point(147, 210);
             this._cb_tipo.Name = "_cb_tipo";
             this._cb_tipo.Size = new System.Drawing.Size(202, 21);
             this._cb_tipo.TabIndex = 62;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Firebrick;
+            this.panel1.Location = new System.Drawing.Point(-5, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(395, 180);
+            this.panel1.TabIndex = 63;
             // 
             // Torneo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(411, 364);
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(389, 364);
             this.Controls.Add(this._cb_tipo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -154,6 +178,7 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.fdd);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Torneo";
@@ -175,5 +200,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox _cb_tipo;
+        private System.Windows.Forms.Panel panel1;
     }
 }
